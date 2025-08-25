eaa0bb67271f5b76f4d5637ef606f7f27417a3be
jaytilokani <jaytilokani25@gmail.com>
2025-08-25 06:42:42 +0530

    Add files via upload

---
+++ c6.hourstominutes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int hours,minutes;
+  printf("enter the number of hours");
+  scanf("%d \n",&hours);
+  minutes=hours*60;
+  printf(" minutes= %d",minutes);
+  return 0;
+}
