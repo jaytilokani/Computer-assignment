217e7885384e32a0e5d7a4cca9d51d058e6b6f0d
jaytilokani <jaytilokani25@gmail.com>
2025-08-25 06:04:27 +0530

    Add files via upload

---
+++ c3.division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf(" enter the first number \n ");
+  scanf("%d " ,&a);
+  printf("enter the second number \n ");
+  scanf("%d ",&b);
+  c=a/b;
+  printf("division of two numbers is  %d",c);
+  return 0;
+}
