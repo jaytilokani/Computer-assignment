c803e5d75772ee245da0cba842cd96c2055768d6
jaytilokani <jaytilokani25@gmail.com>
2025-08-25 07:57:20 +0530

    Add files via upload

---
+++ c8.dollarstorupees.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int dollars, rupees;
+  printf("enter the amount in dollars \n ");
+  scanf( "%d \n", &dollars);
+ rupees= dollars*48;
+ printf("the amount in rupees is %d \n",rupees);
+  return 0;
+}
