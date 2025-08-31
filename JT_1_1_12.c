5e7ed61e89c0d0e2e8e0c2de8a436f71107f340c
jaytilokani <jaytilokani25@gmail.com>
2025-08-31 11:56:18 +0530

    Add files via upload

---
+++ JT_1_1_12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  float grams,kilograms;
+  printf("enter the number of kilograms /n");
+  scanf("%f\n",&kilograms);
+ grams=kilograms*1000;
+ printf("the number of grams =%f\n",grams);
+ return 0;
+ }
